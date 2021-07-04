76d7d361cecfea21e6cdc890264ce268923fdb5d
Uotan <rezer.new@mail.ru>
2021-07-04 13:22:41 +0700

    Добавил время ожидания подключения и просто добавил иконку

--- Assets/C#/PUN2/connecttoserver.cs
+++ Assets/C#/PUN2/connecttoserver.cs
@@ -6,7 +6,7 @@ using Photon.Pun;
 
 public class connecttoserver : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
+    public int _timer;
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
@@ -15,5 +15,15 @@ public class connecttoserver : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         SceneManager.LoadScene(4);
+    }
+    private void FixedUpdate() {
+        _timer++;
+        if (_timer>=1000)
+        {
+            if (!PhotonNetwork.IsConnected)
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
     }
 }
